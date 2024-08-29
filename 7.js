4a9764a965e7d59b7df7fed7625d6928dd833336
Humbucker <90539813+LeonelNV@users.noreply.github.com>
2024-08-28 22:56:11 -0600

    Add files via upload

---
+++ 7.js
@@ -0,0 +1,7 @@
+ //7 - Concatenacion de arreglos
+ function concatenarArreglos(arreglo1, arreglo2){
+    return arreglo1.concat(arreglo2);
+ }
+ let arreglo1 = ["Hola", "Mundo"];
+ let arreglo2 = ["Como", "estan"];
+ console.log("Arreglos concatenados: ", concatenarArreglos(arreglo1, arreglo2));
